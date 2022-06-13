3b939c32347de02e92eb73f94a81fe8d3df97758
Plactrix <48103197+Plactrix@users.noreply.github.com>
2022-06-13 02:15:22 -0700

    Update and rename __resource.lua to fxmanifest.lua
    Renamed resource manifest file and cleaned it up a bit

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version "adamant"
+game "gta5"
+
+author "FivemTools"
+description "A speedometer script for FiveM"
+version "1.0.2"
+
+client_scripts {
+    "config.lua",
+    "cl_carHud.lua"
+}
+
+exports {
+    "ChangeState"
+}
